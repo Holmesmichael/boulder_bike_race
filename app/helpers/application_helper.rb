b39cbd9db26ce4375e7cda3fbf8b584c0a7009f2
Michael A. Holmes <holmes.m.andrew@gmail.com>
2018-06-18 20:12:05 -0500

    styling complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def get_random_avatar
+        image_path_prefix = "app/assets/images/"
+        image_files = Dir.glob("#{image_path_prefix}avatar/*")
+        image_files.sample.split(image_path_prefix)[1]
+      end
+    
+
+
 end
